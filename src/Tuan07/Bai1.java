93ef4a68094acf25203da9141d4ae15bc1d9a9ff
HoangfPhucs <aqqts8@gmail.com>
2024-04-09 16:28:58 +0700

    add

--- src/Tuan07/Bai1.java
+++ src/Tuan07/Bai1.java
@@ -4,6 +4,17 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Vector;
 
 public class Bai1 extends JFrame {
 
@@ -13,7 +24,7 @@ public class Bai1 extends JFrame {
     public Bai1() {
         JFrame frame = new JFrame();
         frame.setTitle("Quản Lý Nhân Viên");
-        frame.setSize(700, 200);
+        frame.setSize(800, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setLayout(new BorderLayout(5,5));
@@ -25,8 +36,54 @@ public class Bai1 extends JFrame {
 
         JPanel pnTieuDe = new JPanel();
         JLabel lblTieuDe = new JLabel("Chọn Bằng Cấp");
-        JComboBox cbbBangCap = new JComboBox();
+
+            BangCapDAO bc = new BangCapDAO();
+        List<BangCap> dsbc = null;
+        try {
+            dsbc = bc.layTatBangCap();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        Vector<String> bangcap = new Vector<>();
+            for (BangCap it : dsbc) {
+                bangcap.add(it.getTenBangCap());
+            }
+            JComboBox<String> cbbBangCap = new JComboBox<>(bangcap);
+
         JButton btnTimKiem = new JButton("Tìm Kiếm");
+        btnTimKiem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                List<NhanVien> dsnv = new ArrayList<>();
+                int selectedIndex = cbbBangCap.getSelectedIndex();
+                String sql = "SELECT * from nhanvien WHERE MaBangCap="+ selectedIndex;
+                JOptionPane.showMessageDialog(null,sql);
+                NhanVienDAO nvdao = new NhanVienDAO();
+                try {
+                    dsnv = nvdao.layTatCaNhanVien(sql);
+                } catch (ClassNotFoundException e1) {
+                    e1.printStackTrace();
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+                String colTieuDe[] = {"Mã NV","Họ Tên","Ngày Sinh","Địa Chỉ","Điện Thoại"};
+                DefaultTableModel model = new DefaultTableModel(colTieuDe,0);
+                Object[] row;
+                for (NhanVien nv : dsnv) {
+                    row = new Object[5];
+                    row[0] = nv.getId();
+                    row[1] = nv.getHoTen();
+                    row[2] = nv.getNgaySinh();
+                    row[3] = nv.getDiaChi();
+                    row[4] = nv.getDienThoai();
+
+                    model.addRow(row);
+                }
+                table.setModel(model);
+            }
+        });
+
         pnTieuDe.setLayout(new FlowLayout());
         pnTieuDe.add(lblTieuDe);
         pnTieuDe.add(cbbBangCap);
@@ -44,9 +101,6 @@ public class Bai1 extends JFrame {
         model.addColumn("Địa Chỉ");
         model.addColumn("Điện Thoại");
 
-        addRow("NV001", "Nguyễn Văn A", "01/01/1990", "Hà Nội", "0123456789");
-        addRow("NV002", "Trần Thị B", "05/05/1995", "Hồ Chí Minh", "0987654321");
-
         JScrollPane scrollPane = new JScrollPane(table);
 
         pnBang.add(scrollPane, BorderLayout.CENTER);
@@ -54,10 +108,151 @@ public class Bai1 extends JFrame {
         frame.setVisible(true);
     }
 
-    private void addRow(String maNV, String hoTen, String ngaySinh, String diaChi, String dienThoai) {
-        Object[] row = {maNV, hoTen, ngaySinh, diaChi, dienThoai};
-        model.addRow(row);
+    public class KetNoiCSDL {
+        private static final String URL = "jdbc:mysql://localhost:3306/db_qlnv";
+        private static final String USER = "root";
+        private static final String PASS = "";
+        public KetNoiCSDL() throws ClassNotFoundException {
+
+        }
+        public static Connection getConnection() throws SQLException, ClassNotFoundException {
+            return DriverManager.getConnection(URL, USER, PASS);
+        }
     }
+
+    public class BangCap {
+        private int MaBangCap;
+        private String TenBangCap;
+
+        public BangCap(int maBangCap, String tenBangCap) {
+            super();
+            MaBangCap = maBangCap;
+            TenBangCap = tenBangCap;
+        }
+
+        public int getMaBangCap() {
+            return MaBangCap;
+        }
+
+        public void setMaBangCap(int maBangCap) {
+            MaBangCap = maBangCap;
+        }
+
+        public String getTenBangCap() {
+            return TenBangCap;
+        }
+
+        public void setTenBangCap(String tenBangCap) {
+            TenBangCap = tenBangCap;
+        }
+    }
+
+    public class BangCapDAO {
+        public List<BangCap> layTatBangCap() throws SQLException, ClassNotFoundException {
+
+            Connection connection = KetNoiCSDL.getConnection();
+            String query = "SELECT * from bangcap";
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            List<BangCap> dsbc = new ArrayList<>();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("MaBangCap");
+                String tenbangcap = resultSet.getString("TenBangCap");
+                BangCap bc = new BangCap(id, tenbangcap);
+                dsbc.add(bc);
+            }
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+            return dsbc;
+        }
+    }
+
+    public class NhanVien {
+        private int id;
+        private String HoTen;
+        private Date NgaySinh;
+        private String DiaChi;
+        private String DienThoai;
+        private int MaBangCap;
+
+        public NhanVien(int id, String hoTen, Date ngaySinh, String diaChi, String dienThoai, int maBangCap) {
+            super();
+            this.id = id;
+            this.HoTen = hoTen;
+            this.NgaySinh = ngaySinh;
+            this.DiaChi = diaChi;
+            this.DienThoai = dienThoai;
+            this.MaBangCap = maBangCap;
+        }
+        public int getId() {
+            return id;
+        }
+        public void setId(int id) {
+            this.id = id;
+        }
+        public String getHoTen() {
+            return HoTen;
+        }
+        public void setHoTen(String hoTen) {
+            HoTen = hoTen;
+        }
+        public Date getNgaySinh() {
+            return NgaySinh;
+        }
+        public void setNgaySinh(Date ngaySinh) {
+            NgaySinh = ngaySinh;
+        }
+        public String getDiaChi() {
+            return DiaChi;
+        }
+        public void setDiaChi(String diaChi) {
+            DiaChi = diaChi;
+        }
+        public String getDienThoai() {
+            return DienThoai;
+        }
+        public void setDienThoai(String dienThoai) {
+            DienThoai = dienThoai;
+        }
+        public int getMaBangCap() {
+            return MaBangCap;
+        }
+        public void setMaBangCap(int maBangCap) {
+            MaBangCap = maBangCap;
+        }
+    }
+
+    public class NhanVienDAO {
+        public List<NhanVien> layTatCaNhanVien(String sql) throws SQLException, ClassNotFoundException {
+            Connection connection = KetNoiCSDL.getConnection();
+            String query = sql;
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            List<NhanVien> dsnv = new ArrayList<>();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("MaNhanVien");
+                String hoTen = resultSet.getString("HoTenNhanVien");
+                Date ngaySinh = resultSet.getDate("NgaySinh");
+                String diaChi = resultSet.getString("DiaChi");
+                String dienThoai = resultSet.getString("DienThoai");
+                int MaBangCap = resultSet.getInt("MaBangCap");
+                NhanVien nv = new NhanVien(id, hoTen, ngaySinh, diaChi, dienThoai, MaBangCap);
+                dsnv.add(nv);
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+            return dsnv;
+        }
+        public NhanVien layTatCaNhanVien(int id) throws SQLException {
+            return null;
+        }
+    }
+
 
     public static void main(String[] args) {
         new Bai1();
